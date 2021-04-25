f79e7fe566b85f0f8949196059de32c5f50be353
mionitsa <ims_3@mail.ru>
2021-04-25 19:38:44 +0300

    work pls

--- api.py
+++ api.py
@@ -49,9 +49,18 @@ def handle_dialog(req, res):
 
     if req['session']['new']:
 
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Мужчина",
+                "Женщина",
+            ]
+        }
+
         res['response']['text'] = 'Здравствуйте! Вас приветствует гид в мир ' \
         'полезной еды. Для начала введите свой пол, возраст, рост и вес. Например: ' \
         'Мужчина 24 175 68.'
+        #res['response']['buttons'] = get_suggests(user_id)
+
 
     if 'мужчина' in req['request']['original_utterance'].lower():
         data = (req['request']['original_utterance']).split()
@@ -63,15 +72,24 @@ def handle_dialog(req, res):
     if 'женщина' in req['request']['original_utterance'].lower():
         data = (req['request']['original_utterance']).split()
         kkal = int(10*int(data[3]) + 6.25*int(data[2]) - 5*int(data[1]))
-        res['response']['text'] = 'Вам нужно потреблять %s килокалорий в день. \n' \
-        'Также, укажите степень Вашей физической активности:\n'\
-        '1. Полное отсутствие физической активности.\n2. Небольшие пробежки или легкая ' \
-        'гимнастика 1-3 раза в неделю.\n3. Средние нагрузки 3-5 раз в неделю.\n4. ' \
-        'Полноценные тренировки 6-7 раз в неделю.\n5. Интенсивные силовые упражнения ' \
-        '2 раза в день.'' % (
+        res['response']['text'] = 'Вам нужно потреблять %s килокалорий в день.' % (
             str(kkal - 161))
         return
+'''
+    res['response']['text'] = 'Также, укажите степень Вашей физической активности:\n'\
+    '1. Полное отсутствие физической активности.\n2.Небольшие пробежки или легкая ' \
+    'гимнастика 1-3 раза в неделю.\n3. Средние нагрузки 3-5 раз в неделю.\n4. ' \
+    'Полноценные тренировки 6-7 раз в неделю.\n5. Интенсивные силовые физические упражнения ' \
+    '2 раза в день.'
+'''
 
-    if req['request']['original_utterance'].lower() == '2':
-        res['response']['text'] = 'ЧТО-ТО'
-        return
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+
+    suggests = [
+        {'title': suggest, 'hide': False}
+        for suggest in session['suggests']
+    ]
+
+
+    return suggests
