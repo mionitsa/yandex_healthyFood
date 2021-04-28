59d3dba4ca3ec6fb7a3c2bbf7357bb190caf8017
mionitsa <ims_3@mail.ru>
2021-04-28 15:15:27 +0300

    . - ,

--- api.py
+++ api.py
@@ -62,18 +62,23 @@ def handle_dialog(req, res):
         return
 
     if 'мужчина' in req['request']['original_utterance'].lower():
+
+        if '.' in req['request']['original_utterance']:
+            req['request']['original_utterance'] = req['request']['original_utterance'].replace('.',' '))
+        if ',' in req['request']['original_utterance']:
+            req['request']['original_utterance'] = req['request']['original_utterance'].replace(',',' '))
+        if '-' in req['request']['original_utterance']:
+            req['request']['original_utterance'] = req['request']['original_utterance'].replace('-',' '))
+
         data = (req['request']['original_utterance']).split()
-        new_data = []
-        for i in range(len(data)):
-            new_data.append(data[i].replace('.',''))
-        kkal = int(10*int(new_data[3]) + 6.25*int(new_data[2]) - 5*int(new_data[1]) + 5)
-        if int(new_data[4]) == 1:
+        kkal = int(10*int(data[3]) + 6.25*int(data[2]) - 5*int(data[1]) + 5)
+        if int(data[4]) == 1:
             kkal *= 1.2
-        if int(new_data[4]) == 2:
+        if int(data[4]) == 2:
             kkal *= 1.375
-        if int(new_data[4]) == 3:
+        if int(data[4]) == 3:
             kkal *= 1.55
-        if int(new_data[4]) == 4:
+        if int(data[4]) == 4:
             kkal *= 1.725
         res['response']['text'] = 'Вам нужно потреблять %s килокалорий в день.' % (
             str(int(kkal)))
@@ -102,18 +107,23 @@ def handle_dialog(req, res):
         return
 
     if 'женщина' in req['request']['original_utterance'].lower():
+
+        if '.' in req['request']['original_utterance']:
+            req['request']['original_utterance'] = req['request']['original_utterance'].replace('.',' '))
+        if ',' in req['request']['original_utterance']:
+            req['request']['original_utterance'] = req['request']['original_utterance'].replace(',',' '))
+        if '-' in req['request']['original_utterance']:
+            req['request']['original_utterance'] = req['request']['original_utterance'].replace('-',' '))
+            
         data = (req['request']['original_utterance']).split()
-        new_data = []
-        for i in range(len(data)):
-            new_data.append(data[i].replace('.',''))
-        kkal = int(10*int(new_data[3]) + 6.25*int(new_data[2]) - 5*int(new_data[1]) - 161)
+        kkal = int(10*int(data[3]) + 6.25*int(data[2]) - 5*int(data[1]) - 161)
         if int(new_data[4]) == 1:
             kkal *= 1.2
-        if int(new_data[4]) == 2:
+        if int(data[4]) == 2:
             kkal *= 1.375
-        if int(new_data[4]) == 3:
+        if int(data[4]) == 3:
             kkal *= 1.55
-        if int(new_data[4]) == 4:
+        if int(data[4]) == 4:
             kkal *= 1.725
         res['response']['text'] = 'Вам нужно потреблять %s килокалорий в день.' % (
             str(int(kkal)))
